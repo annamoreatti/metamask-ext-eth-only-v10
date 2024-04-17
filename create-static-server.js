0b34c6cf081062fb4321f1c4d2e594db94bbb598
annamoreatti <162908400+annamoreatti@users.noreply.github.com>
2024-04-17 09:11:44 -0400

    Update create-static-server.js

--- create-static-server.js
+++ create-static-server.js
@@ -7,7 +7,7 @@ const serveHandler = require('serve-handler');
 const createStaticServer = (rootDirectory) => {
   return http.createServer((request, response) => {
     if (request.url.startsWith('/node_modules/')) {
-      request.url = request.url.substr(18);
+      request.url = request.url.substr(20);
       return serveHandler(request, response, {
         directoryListing: false,
         public: path.resolve('./node_modules'),
